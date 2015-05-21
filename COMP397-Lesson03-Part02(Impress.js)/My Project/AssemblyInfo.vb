92ebd071c57637a82acce29671e5774ce127ae5c
Ruksharpatel <rukkhsarpatel555@gmail.com>
2015-05-20 23:13:30 -0400

    setupbasicFramework

---
+++ COMP397-Lesson03-Part02(Impress.js)/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+<Assembly: AssemblyTitle("COMP397_Lesson03_Part02_Impress.js_")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("Toshiba")> 
+<Assembly: AssemblyProduct("COMP397_Lesson03_Part02_Impress.js_")> 
+<Assembly: AssemblyCopyright("Copyright © Toshiba 2015")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("1709c1d1-f427-42f6-bd99-aa7698bff195")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
